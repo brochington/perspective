befba572fff24b0ff4409452577816701bad7f35
Broch Stilley <brochington@gmail.com>
2022-12-09 12:36:40 -0800

    Adding some unit tests.

--- packages/perspective-viewer-d3fc/src/js/tooltip/generateHTML.js
+++ packages/perspective-viewer-d3fc/src/js/tooltip/generateHTML.js
@@ -14,6 +14,7 @@ export function generateHtml(tooltipDiv, data, settings) {
     const tooltipValues = getGroupValues(data, settings)
         .concat(getSplitValues(data, settings))
         .concat(getDataValues(data, settings));
+
     addDataValues(tooltipDiv, tooltipValues);
 }
 
